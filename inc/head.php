9cfd64f14a1385c7f29e2151e8004af37b948e9b
Maxime Charpentier <maximecharpentier0@gmail.com>
2018-03-10 17:33:18 +0100

    Add global.css for social and wrapper / Add nespresso.css

--- inc/head.php
+++ inc/head.php
@@ -4,6 +4,6 @@
         <title><?php echo $title; ?></title>
         <meta name="description" content="<?php echo $description; ?>">
         <link href="https://fonts.googleapis.com/css?family=Playfair+Display:700|Roboto:300,400,500,700,900" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css?family=Roboto+Mono:400,500,700" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Roboto+Mono:400" rel="stylesheet">
         <link rel="stylesheet" href="../assets/css/style.css">
     </head>
