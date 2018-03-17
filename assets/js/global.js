72513b3fcb868796475eceebff08ea9fe97ac7b0
Maxime Charpentier <maximecharpentier0@gmail.com>
2018-03-18 00:53:10 +0100

    Add color change on header when on project page

--- assets/js/global.js
+++ assets/js/global.js
@@ -8,6 +8,7 @@ var brand         = document.querySelector('.brand');
 var brandLink     = document.querySelector('.brand__link');
 var brandLinkBold = document.querySelector('.brand__link__bold');
 var headerButton  = document.querySelector('.header__button');
+var menuLink      = document.querySelectorAll('.menu__link');
 var modal         = document.querySelector('.modal');
 var modalLink     = document.querySelectorAll('.modal__link');
 
@@ -19,9 +20,25 @@ headerButton.addEventListener('click', function () {
     if (headerButton.classList.contains('header__button--active')) {
         headerButton.classList.remove('header__button--active');
         modal.classList.remove('modal--active');
+        if (body.classList.contains('body--project')) {
+            brandLink.classList.add('brand__link--project');
+            brandLinkBold.classList.add('brand__link__bold--project');
+            headerButton.classList.add('header__button--project');
+            for (var i = 0; i < menuLink.length; i++) {
+                menuLink[i].classList.add('menu__link--project');
+            }
+        }
     } else {
         headerButton.classList.add('header__button--active');
         modal.classList.add('modal--active');
+        if (body.classList.contains('body--project')) {
+            brandLink.classList.remove('brand__link--project');
+            brandLinkBold.classList.remove('brand__link__bold--project');
+            headerButton.classList.remove('header__button--project');
+            for (var i = 0; i < menuLink.length; i++) {
+                menuLink[i].classList.remove('menu__link--project');
+            }
+        }
     }
 });
 
@@ -33,23 +50,6 @@ window.addEventListener('scroll', function() {
     header.style.position = 'absolute';
     header.style.top = '0px';
     header.style.left = '0px';
-});
-
-/*
-Brand hover
- */
-
-brand.addEventListener('mouseover', function() {
-    if (window.innerWidth > 1025) {
-        brandLink.classList.add('brand__link--active');
-        brandLinkBold.classList.add('brand__link__bold--active');
-    }
-});
-brand.addEventListener('mouseout', function() {
-    if (window.innerWidth > 1025) {
-        brandLink.classList.remove('brand__link--active');
-        brandLinkBold.classList.remove('brand__link__bold--active');
-    }
 });
 
 /*
@@ -66,9 +66,45 @@ for (var i = 0; i < modalLink.length; i++) {
         modalLink[0].classList.remove('modal__link--active');
         modalLink[1].classList.add('modal__link--active');
     }
-    else {
+    if (body.classList.contains('body--project')) {
         modalLink[0].classList.remove('modal__link--active');
         modalLink[1].classList.remove('modal__link--active');
     }
 }
+
+/*
+Change header color when on project page
+ */
+
+if (body.classList.contains('body--project')) {
+    brandLink.classList.add('brand__link--project');
+    brandLinkBold.classList.add('brand__link__bold--project');
+    headerButton.classList.add('header__button--project');
+    for (var i = 0; i < menuLink.length; i++) {
+        menuLink[i].classList.add('menu__link--project');
+    }
+}
+
+/*
+Brand hover
+ */
+
+brand.addEventListener('mouseover', function() {
+    if (window.innerWidth > 1025) {
+        if (body.classList.contains('body--project'))
+            brandLink.classList.remove('brand__link--project');
+            brandLinkBold.classList.remove('brand__link__bold--project');
+        brandLink.classList.add('brand__link--active');
+        brandLinkBold.classList.add('brand__link__bold--active');
+    }
+});
+brand.addEventListener('mouseout', function() {
+    if (window.innerWidth > 1025) {
+        brandLink.classList.remove('brand__link--active');
+        brandLinkBold.classList.remove('brand__link__bold--active');
+        if (body.classList.contains('body--project'))
+            brandLink.classList.add('brand__link--project');
+            brandLinkBold.classList.add('brand__link__bold--project');
+    }
+});
 
