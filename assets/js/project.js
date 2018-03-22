947137cb80a42162f4c77f7f08a2dc2a85ecd58e
Maxime Charpentier <maximecharpentier0@gmail.com>
2018-03-22 14:11:22 +0100

    Add anchors to project pages summary

--- assets/js/project.js
+++ assets/js/project.js
@@ -6,7 +6,6 @@ var sectionLink = document.querySelector('.section__link');
 
 sectionLink.addEventListener('mouseover', function () {
     if (window.innerWidth > 1025) {
-        console.log('coucou')
         sectionLink.style.transition = '3s ease';
         sectionLink.classList.add('section__link--appear');
         setTimeout(function() {
@@ -19,3 +18,49 @@ sectionLink.addEventListener('mouseover', function () {
         }, 700);
     }
 });
+
+var sectionButton = document.querySelectorAll('.section__button');
+var section = document.querySelectorAll('.section--counter');
+
+/*
+(Slider) Nav button anchor scroll on click
+ */
+
+for (var i = 0; i < sectionButton.length; i++) {
+    sectionButton[0].addEventListener('click', function() {
+        section[0].scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+        });
+    });
+    sectionButton[1].addEventListener('click', function() {
+        section[1].scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+        });
+    });
+    sectionButton[2].addEventListener('click', function() {
+        section[2].scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+        });
+    });
+    sectionButton[3].addEventListener('click', function() {
+        section[3].scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+        });
+    });
+    sectionButton[4].addEventListener('click', function() {
+        section[4].scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+        });
+    });
+    sectionButton[5].addEventListener('click', function() {
+        section[5].scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+        });
+    });
+}
