4d08d0f9a409507a479598b4be6d29c69195324c
Maxime Charpentier <maximecharpentier0@gmail.com>
2018-03-27 11:07:42 +0200

    Fix section color change on project pages

--- spotnride/index.php
+++ spotnride/index.php
@@ -45,7 +45,7 @@
             <div class="section__wrapper">
                 <h3 class="section__title">Fonctionnalités</h3>
                 <p class="section__text">L'objectif était de réaliser une application la plus simple d'utilisation possible. Pour cela, nous avons pensé l'application autour des fonctionnalités principales.</p>
-                <video class="section__image" src="../assets/img/filmatics/slider.mp4" autoplay="true" loop muted></video>
+                <img class="section__image section__image--noshadow" src="../assets/img/spotnride/sketches.png"></img>
                 <h4 class="section__subtitle">Map</h4>
                 <p class="section__text">Nous avons opter pour un système de carte pour la recherche de spot, qui faisait également office de page d'accueil de l'application. L'idée étant de pouvoir zoomer et dézoomer afin de trouver le spot qu'il vous convient, et de pouvoir suivre en temps réel vos riders favoris via un filtre sélectionner en haut de l'écran.</p>
                 <ul class="section__list">
@@ -53,23 +53,23 @@
                     <li class="section__item section__item--text">Accéder à la page d'un spot</li>
                     <li class="section__item section__item--text">Suivis des riders favoris en temps réel</li>
                 </ul>
-                <img class="section__image" src="../assets/img/spotnride/map.jpg" alt="Image des fiches de films spotnride">
+                <img class="section__image section__image--spotnride" src="../assets/img/spotnride/map.jpg" alt="Image des fiches de films spotnride">
                 <h4 class="section__subtitle">Recherche avancée</h4>
                 <p class="section__text">Toujours sur l'onglet map, l'utilisateur à la possibilité de faire une recherche de spot avancée selon plusieurs critères.</p>
-                <img class="section__image" src="../assets/img/spotnride/filters.jpg" alt="Image des fiches de films spotnride">
+                <img class="section__image section__image--spotnride" src="../assets/img/spotnride/filters.jpg" alt="Image des fiches de films spotnride">
                 <h4 class="section__subtitle">Page de profil</h4>
                 <p class="section__text">Ce projet à été réalisé en groupe de 5 dans le cadre d'une semaine intensive Intégration à HETIC. Nous avions eu les consignes le lundi, et nous devions présenter notre projet le vendredi matin. Nous avions donc quatres jours pour réaliser une landing page sur un concept de notre choix. Le thème à respecté était "urbain mais classe", ce qui nous à diriger vers ce projet d'application mobile pour les riders.</p>
-                <img class="section__image" src="../assets/img/spotnride/spot.jpg" alt="Image des fiches de films spotnride">
+                <img class="section__image section__image--spotnride" src="../assets/img/spotnride/spot.jpg" alt="Image des fiches de films spotnride">
                 <h4 class="section__subtitle">Page des spots</h4>
                 <p class="section__text">Ce projet à été réalisé en groupe de 5 dans le cadre d'une semaine intensive Intégration à HETIC. Nous avions eu les consignes le lundi, et nous devions présenter notre projet le vendredi matin. Nous avions donc quatres jours pour réaliser une landing page sur un concept de notre choix. Le thème à respecté était "urbain mais classe", ce qui nous à diriger vers ce projet d'application mobile pour les riders.</p>
-                <img class="section__image" src="../assets/img/spotnride/profil.jpg" alt="Image des fiches de films spotnride">
+                <img class="section__image section__image--spotnride" src="../assets/img/spotnride/profil.jpg" alt="Image des fiches de films spotnride">
             </div>
         </section>
         <section class="section section--counter">
             <div class="section__wrapper">
                 <h3 class="section__title">Landing page</h3>
                 <p class="section__text">Ce projet à été réalisé en groupe de 5 dans le cadre d'une semaine intensive Intégration à HETIC. Nous avions eu les consignes le lundi, et nous devions présenter notre projet le vendredi matin. Nous avions donc quatres jours pour réaliser une landing page sur un concept de notre choix. Le thème à respecté était "urbain mais classe", ce qui nous à diriger vers ce projet d'application mobile pour les riders.</p>
-                <img class="section__image" src="../assets/img/spotnride/profil.jpg" alt="Image des fiches de films spotnride">
+                <img class="section__image section__image--shadow" src="../assets/img/spotnride/profil.jpg" alt="Image des fiches de films spotnride">
                 <a class="section__link section__link--bottom" href="">Voir le site</a>
             </div>
         </section>
